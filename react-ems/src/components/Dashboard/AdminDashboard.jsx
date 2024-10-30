b9d8a66263e285cb1d02e330e26e8ab70859eeaf
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-10-30 23:21:14 +0530

    Admin Dashboard almost Completed

--- react-ems/src/components/Dashboard/AdminDashboard.jsx
+++ react-ems/src/components/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "./../Others/Header";
+import CreateTask from './../Others/CreateTask';
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
-  )
-}
+    <div className="h-screen w-full p-10">
+      <Header />
+      <CreateTask />
+    </div>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
---
+++ react-ems/src/components/Others/AllTasks.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AllTasks = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5'>
+        <div>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTasks
