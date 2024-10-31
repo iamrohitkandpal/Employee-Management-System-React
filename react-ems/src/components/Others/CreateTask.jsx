8a6a5cbe781e814718cdb554ddfa44b53713ba11
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-10-31 23:53:43 +0530

    Updating the button styling

--- react-ems/src/components/Others/CreateTask.jsx
+++ react-ems/src/components/Others/CreateTask.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CreateTask = () => {
   return (
-    <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
+    <div className="p-5 bg-[#1c1c1c] mt-7 rounded-lg">
         <form className="flex w-full flex-wrap items-start justify-between">
           <div className="w-1/2">
             <div>
@@ -26,9 +26,8 @@ const CreateTask = () => {
           <div className="w-2/5 flex flex-col items-start">
             <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
             <textarea name="" className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400" cols="30" rows="10"></textarea>
-            <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">Create Task</button>
+            <button className="transition-all duration-700 font-bold ease-in-out bg-sky-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">Create Task</button>
           </div>
-
         </form>
       </div>
   )
