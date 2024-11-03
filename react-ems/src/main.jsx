fa2c13af975195ee70051f3ae1b3dd1c58c654af
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-03 20:46:15 +0530

    Writing logic for data storage and usage theough local storage

--- react-ems/src/main.jsx
+++ react-ems/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import AuthContext from './context/AuthContext';
+import TaskContext from './context/TaskContext';
 
+localStorage.clear();
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <AuthContext>
+      <TaskContext>
+        <App />
+      </TaskContext>
+    </AuthContext>
+  </React.StrictMode>,
+);
