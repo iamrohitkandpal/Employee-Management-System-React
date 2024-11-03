e69d070fa97a868f8b60f9219d06681c6a114cde
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-03 20:45:54 +0530

    All the React Frontend part completed

--- react-ems/src/components/Others/AllTasks.jsx
+++ react-ems/src/components/Others/AllTasks.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const AllTasks = () => {
   return (
-    <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
-        <div className='bg-red-400 py-2 px-4 flex justify-between'>
+    <div className='bg-[#1c1c1c] p-5 h-64 overflow-auto rounded-lg mt-5'>
+        <div className='bg-red-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-green-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
+            <h2>Rohit</h2>
+            <h3>Make a Retro Webpage</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
             <h2>Rohit</h2>
             <h3>Make a Retro Webpage</h3>
             <h5>Status</h5>
