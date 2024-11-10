db968412d8e9fd57063bc130db3c4baa93208fe0
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-10 23:59:16 +0530

    Making them dynamic

--- react-ems/src/components/Others/TaskNumber.jsx
+++ react-ems/src/components/Others/TaskNumber.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TaskNumber = ({ data }) => {
   return (
     <div className="flex screen mt-10 justify-between gap-5">
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-red-500">
+      <div className="rounded-xl px-10 py-12 w-[45%] bg-yellow-500">
         <h2 className="text-2xl font-semibold">{data.taskCounts.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
@@ -11,11 +11,11 @@ const TaskNumber = ({ data }) => {
         <h2 className="text-2xl font-semibold">{data.taskCounts.completed}</h2>
         <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-cyan-500">
+      <div className="rounded-xl px-10 py-12 w-[45%] bg-indigo-600">
         <h2 className="text-2xl font-semibold">{data.taskCounts.active}</h2>
         <h3 className="text-xl font-medium">Accepted Task</h3>
       </div>
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-violet-500">
+      <div className="rounded-xl px-10 py-12 w-[45%] bg-red-500">
         <h2 className="text-2xl font-semibold">{data.taskCounts.failed}</h2>
         <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
@@ -25,3 +25,4 @@ const TaskNumber = ({ data }) => {
 
 export default TaskNumber;
 
+ 
--- react-ems/src/components/Task List/TaskList.jsx
+++ react-ems/src/components/Task List/TaskList.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import AcceptedTask from './AcceptedTask'
+import NewTask from './NewTask'
+import CompletedTask from './CompletedTask';
+import FailedTask from './FailedTask';
 
 const TaskList = ({data}) => {
+  console.log(data);
+  
   return (
     <div id='tasklist' className=' flex items-center justify-start gap-7 flex-nowrap h-[55%] py-5  overflow-x-auto w-full rounded-xl mt-10'>
-        {data.tasks.map(elem) => {
-          if(elem.active){
-            
+        {data.tasks.map((task) => {
+          if(task.active && !task.newTask){
+            return <AcceptedTask data={task}/>
           }
-        }}
+          if(task.newTask){
+            return <NewTask data={task}/>
+          }
+          if(task.completed){
+            return <CompletedTask data={task}/>
+          }
+          if(task.failed){
+            return <FailedTask data={task}/>
+          }
+        })}
     </div>
   )
 }
