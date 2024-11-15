ad0ccc94528e548b305ae608bdf124fe5338be7c
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-15 20:47:26 +0530

    Added Logout Feature and Dynamic Usernames

--- react-ems/src/components/Others/Header.jsx
+++ react-ems/src/components/Others/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = ({data}) => {
+const Header = (props) => {
+  
+  const logOutUser = () => {
+    localStorage.setItem('loggedUser', '');
+    props.changeUser('');
+  }
+
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{data.firstName} 🎉</span></h1>
-        <button className='bg-red-500 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{props.data.firstName} 🎉</span></h1>
+        <button onClick={logOutUser} className='bg-red-500 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
     </div>
   )
 }
