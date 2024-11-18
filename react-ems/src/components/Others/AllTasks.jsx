bf3fab6607476415d88b7619915b35fbe45b910c
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-18 22:42:20 +0530

    Created Admin Dashboard Sub Component with dynamic data filling

--- react-ems/src/components/Others/AllTasks.jsx
+++ react-ems/src/components/Others/AllTasks.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "./../../context/AuthProvider";
 
 const AllTasks = () => {
+  const authData = useContext(AuthContext);
+  console.log(authData);
+  
   return (
-    <div className='bg-[#1c1c1c] p-5 h-64 overflow-auto rounded-lg mt-5'>
-        <div className='bg-red-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-blue-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-green-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-violet-400 rounded-md mt-4 py-2 px-4 flex justify-between'>
-            <h2>Rohit</h2>
-            <h3>Make a Retro Webpage</h3>
-            <h5>Status</h5>
-        </div>
+    <div className="bg-[#1c1c1c] p-6 h-72 rounded-lg mt-5">
+      <div className="bg-zinc-700 rounded-md py-2 px-4 flex justify-between items-center">
+        <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-medium w-1/5">New Task</h3>
+        <h3 className="text-lg font-medium w-1/5">Accepted Task</h3>
+        <h3 className="text-lg font-medium w-1/5">Completed Task</h3>
+        <h3 className="text-lg font-medium w-1/5">Failed Task</h3>
+      </div>
+
+      <div className="h-[80%] overflow-auto">
+          {authData[0].employees.map(function (elem, index) {
+            return (
+              <div key={index} className="bg-zinc-600 rounded-md mt-4 py-2 px-4 flex justify-between items-center">
+                <h2 className="text-lg font-medium w-1/5">{elem.firstName}</h2>
+                <h3 className="text-lg font-medium w-1/5">{elem.taskCounts.newTask}</h3>
+                <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.active}</h5>
+                <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.completed}</h5>
+                <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.failed}</h5>
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AllTasks;
