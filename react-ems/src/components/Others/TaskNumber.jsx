171203e72a16eec074e15ef83d55285493805046
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-18 22:44:13 +0530

    Changed the styling of employee task number component

--- react-ems/src/components/Others/TaskNumber.jsx
+++ react-ems/src/components/Others/TaskNumber.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const TaskNumber = ({ data }) => {
   return (
     <div className="flex screen mt-10 justify-between gap-5">
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-yellow-500">
+      <div className="rounded-xl px-10 py-12 pb-10 border-b-8 border-yellow-500 w-[45%] bg-zinc-700">
         <h2 className="text-2xl font-semibold">{data.taskCounts.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-emerald-500">
+      <div className="rounded-xl px-10 py-12 pb-10 border-b-8 border-emerald-500 w-[45%] bg-zinc-700">
         <h2 className="text-2xl font-semibold">{data.taskCounts.completed}</h2>
         <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-indigo-600">
+      <div className="rounded-xl px-10 py-12 pb-10 border-b-8 border-sky-500 w-[45%] bg-zinc-700">
         <h2 className="text-2xl font-semibold">{data.taskCounts.active}</h2>
         <h3 className="text-xl font-medium">Accepted Task</h3>
       </div>
-      <div className="rounded-xl px-10 py-12 w-[45%] bg-red-500">
+      <div className="rounded-xl px-10 py-12 pb-10 border-b-8 border-red-500 w-[45%] bg-zinc-700">
         <h2 className="text-2xl font-semibold">{data.taskCounts.failed}</h2>
         <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
