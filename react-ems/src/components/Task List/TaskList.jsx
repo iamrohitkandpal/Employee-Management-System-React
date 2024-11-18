447468447c3597a16df9092fa640520f00550410
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-18 22:45:11 +0530

    TaskList Data Passing Structure from this component to Sub-components

--- react-ems/src/components/Task List/TaskList.jsx
+++ react-ems/src/components/Task List/TaskList.jsx
@@ -7,18 +7,19 @@ import FailedTask from './FailedTask';
 const TaskList = ({data}) => {
   return (
     <div id='tasklist' className=' flex items-center justify-start gap-7 flex-nowrap h-[55%] py-5  overflow-x-auto w-full rounded-xl mt-10'>
-        {data.tasks.map((task) => {
+        {data.tasks.map((task, index) => {
+          
           if(task.active && !task.newTask){
-            return <AcceptedTask data={task}/>
+            return <AcceptedTask data={task} key={index}/>
           }
           if(task.newTask){
-            return <NewTask data={task}/>
+            return <NewTask data={task} key={index}/>
           }
           if(task.completed){
-            return <CompletedTask data={task}/>
+            return <CompletedTask data={task} key={index}/>
           }
           if(task.failed){
-            return <FailedTask data={task}/>
+            return <FailedTask data={task} key={index}/>
           }
         })}
     </div>
