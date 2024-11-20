2bb83f8e7ffd8a41ccf5186242a864654cd953eb
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-20 23:44:39 +0530

    Creating Admin Backend Management

--- react-ems/src/components/Others/AllTasks.jsx
+++ react-ems/src/components/Others/AllTasks.jsx
@@ -6,7 +6,7 @@ const AllTasks = () => {
   console.log(authData);
   
   return (
-    <div className="bg-[#1c1c1c] p-6 h-72 rounded-lg mt-5">
+    <div className="bg-[#1c1c1c] p-6 rounded-lg mt-5">
       <div className="bg-zinc-700 rounded-md py-2 px-4 flex justify-between items-center">
         <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
         <h3 className="text-lg font-medium w-1/5">New Task</h3>
@@ -15,7 +15,7 @@ const AllTasks = () => {
         <h3 className="text-lg font-medium w-1/5">Failed Task</h3>
       </div>
 
-      <div className="h-[80%] overflow-auto">
+      <div className="overflow-auto">
           {authData[0].employees.map(function (elem, index) {
             return (
               <div key={index} className="bg-zinc-600 rounded-md mt-4 py-2 px-4 flex justify-between items-center">
@@ -24,7 +24,7 @@ const AllTasks = () => {
                 <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.active}</h5>
                 <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.completed}</h5>
                 <h5 className="text-lg font-medium w-1/5">{elem.taskCounts.failed}</h5>
-              </div>
+              </div> 
             );
           })}
       </div>
--- react-ems/src/components/Others/CreateTask.jsx
+++ react-ems/src/components/Others/CreateTask.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateTask = () => {
+
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescripition, setTaskDescription] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [firstName, setFirstName] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("Task Completed");
+  }
+
   return (
     <div className="p-5 bg-[#1c1c1c] mt-7 rounded-lg">
-        <form className="flex w-full flex-wrap items-start justify-between">
+        <form onSubmit={(e)=>{
+          submitHandler(e);
+        }} className="flex w-full flex-wrap items-start justify-between">
           <div className="w-1/2">
             <div>
               <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
-              <input type="text" className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" placeholder="Make a UI Design" />
+              <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-4/5 py-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" placeholder="Make a UI Design" />
             </div>
             <div>
               <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
-              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="date" />
+              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-4/5 py-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" type="date" />
             </div>
             <div>
               <h3 className="text-sm text-gray-300 mb-0.5">Assign To</h3>
-              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Employee Name" />
+              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-4/5 py-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" type="text" placeholder="Employee Name" />
             </div>
             <div>
-              <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
-              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Design, Dev, etc" />
+              <h3 className="text-sm text-gray-300 mb-0.5">Piority</h3>
+              <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-4/5 py-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose Priority</option>
+                <option value="Low">Low</option>
+                <option value="Mid">Medium</option>
+                <option value="High">High</option>
+                <option value="Urgent">Urgent</option>
+              </select>
             </div>
+            {/* <div>
+              <h3 className="text-sm text-gray-300 mb-0.5">Piority</h3>
+              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="dropdown" placeholder="Design, Dev, etc" />
+            </div> */}
           </div>
 
           <div className="w-2/5 flex flex-col items-start">
